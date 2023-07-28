28932e78dd68bc50f6316329faab54069cb7a125
gdavis-21 <gdavis19@uab.edu>
2023-07-28 13:16:09 -0500

    Modify vite.config to include correct base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/runplan-frontend/',
+  base: './runplan-frontend/',
   plugins: [
     vue(),
   ],
