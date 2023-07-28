c81f781f0c333c529e3cedfb18ed65d68cb6b454
gdavis-21 <gdavis19@uab.edu>
2023-07-28 14:30:51 -0500

    Change base URL to be correct

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import App from './App.vue'
 
 let DEBUG = false;
 let BASE_URL = ""
-BASE_URL = DEBUG ? "http://127.0.0.1:8000" : "https://run-plan-app-cd5c57bf131d.herokuapp.com"
+BASE_URL = DEBUG ? "http://127.0.0.1:8000" : "https://run-plan-backend-32d52423f1dc.herokuapp.com"
 export { BASE_URL }
    
 
