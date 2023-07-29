09119480f665c75725afc9e4e6f119f3c966d75d
gdavis-21 <gdavis19@uab.edu>
2023-07-29 14:35:11 -0500

    Change debug to false

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import router from '/src/router/index.js'
 import App from './App.vue'
 
-let DEBUG = true;
+let DEBUG = false;
 let BASE_URL = ""
 BASE_URL = DEBUG ? "http://127.0.0.1:8000" : "https://run-plan-backend-32d52423f1dc.herokuapp.com"
 export { BASE_URL }
