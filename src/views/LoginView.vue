5033c5fdd6c9260c299216e74c86e1963b9d454a
gdavis-21 <gdavis19@uab.edu>
2023-07-29 15:07:48 -0500

    Rename X-Csrftoken to X-CSRFToken

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -15,7 +15,7 @@ async function submitUserLogin(e) {
     const parser = new DOMParser()
     const dom = parser.parseFromString(text, "text/html")
     const csrfToken = dom.querySelector("input").value
-    document.cookie = `csrftoken=${csrfToken}; path=/; SameSite=None`;
+    document.cookie = `csrftoken=${csrfToken}; path=/; SameSite=None; Secure=True`;
 
     try {
         const formData = new FormData()
