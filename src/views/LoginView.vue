8e8a2eb3fe0bd946f6b1a383b4f0e16fb04a8a3f
gdavis-21 <gdavis19@uab.edu>
2023-08-03 15:23:24 -0500

    Update style of login page, add password and create account (still need to be implemented

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,13 +6,15 @@ import { BASE_URL } from '/src/main.js'
 const username = ref("")
 const password = ref("")
 
+const submitButtonOpacity = ref(1)
+
 async function submitUserLogin(e) {
     e.preventDefault()
-    const csrfTokenResponse = await fetch(`${BASE_URL}` + "/fetchCSRFToken/", {
-        credentials: "include"
-    })
-    const csrfToken = await csrfTokenResponse.text()
-    document.cookie
+    // const csrfTokenResponse = await fetch(`${BASE_URL}` + "/fetchCSRFToken/", {
+    //     credentials: "include"
+    // })
+    // const csrfToken = await csrfTokenResponse.text()
+    // document.cookie
     // const parser = new DOMParser()
     // const dom = parser.parseFromString(text, "text/html")
     // const csrfToken = dom.querySelector("input").value
@@ -23,14 +25,14 @@ async function submitUserLogin(e) {
         formData.append("username", username.value)
         formData.append("password", password.value)
 
-        const headers = {
-            "X-CSRFToken": csrfToken,
-        }
+        // const headers = {
+        //     "X-CSRFToken": csrfToken,
+        // }
 
         const response = await fetch(`${BASE_URL}` + "/authenticateUser/", {
             method: "POST",
             body: formData,
-            headers: headers,
+            // headers: headers,
             credentials: "include"
         })
         // If success, redirect user to the dashboard.
@@ -46,70 +48,81 @@ async function submitUserLogin(e) {
     }
 }
 
+
+
 </script>
 
 <template>
-    <div class="login-container">
-        <div class="runplan-logo-container">
-            <h1 class="runplan-logo">Run Plan</h1>
+    <div class="container">
+        <div class="login-container">
+            <div class="runplan-logo-container">
+                <h1 class="runplan-logo text">Run Plan</h1>
+            </div>
+            <div class="title-container">
+                <h2 class="title text" style="text-align: center;">Sign In.</h2>
+            </div>
+            <form class="text" id="sign-in" @submit="submitUserLogin" method="POST">
+                <div class="username-container">
+                    <label class="username-label" for="username">Username*</label>
+                    <input class="username-input text" type="input" id="username" name="username" v-model="username" autocomplete="off">
+                </div>
+                <div class="password-container">
+                    <label class="password-label" for="password">Password*</label>
+                    <input class="password-input text" type="password" id="password" name="password" v-model="password" autocomplete="off">
+                </div>
+                <div class="password-help-container">
+                    <p class="password-help">Password Help?</p>
+                </div>
+                <div class="remember-me-container">
+                    <input class="remember-me-checkbox" type="checkbox" id="checkbox" name="rememberPassword" value="checked">
+                    <label class="remember-me-label" for="rememberPassword">Remember Me</label>
+                </div>
+                <button type="submit" class="sign-in-button" :style="{ opacity: submitButtonOpacity }" @mousedown="submitButtonOpacity = 0.75" @mouseup="submitButtonOpacity = 1">Sign In</button>
+                <div class="create-account-container">
+                    <p>Don't Have an Account?</p>
+                    <p class="create-account">Create One Here!</p>
+                </div>
+            </form>
         </div>
-        <div class="title-container">
-            <h2 class="title text" style="text-align: center;">Sign In.</h2>
-        </div>
-        <form class="text" id="sign-in" @submit="submitUserLogin" method="POST">
-            <div class="username-container">
-                <label class="username-label" for="username">Username*</label>
-                <input class="username-input" type="input" id="username" name="username" v-model="username" autocomplete="off">
-            </div>
-            <div class="password-container">
-                <label class="password-label" for="password">Password*</label>
-                <input class="password-input" type="password" id="password" name="password" v-model="password" autocomplete="off">
-            </div>
-            <div class="remember-me-container">
-                <input class="remember-me-checkbox" type="checkbox" id="checkbox" name="rememberPassword" value="checked">
-                <label class="remember-me-label" for="rememberPassword">Remember Me</label>
-            </div>
-            <button type="submit" class="sign-in-button">Sign In</button>
-        </form>
     </div>
 </template>
 
 
 <style scoped>
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap');
+
     .text {
         font-family: "Montserrat";
+        color: white;
     }
-    .login-container {
+    .container {
         display: flex;
         flex-direction: column;
         align-items: center;
-        justify-content: center;
-        justify-content: center;
-
-        position: relative;
-        left: 10%;
-        top: 10%;
-        width:80%;
-        height:80%;
-    }
-    .title-container {
-        text-align: center;
+        justify-content: start;
         width:100%;
+        height:100%;
+        background-color: #0387ba;
     }
-    .title {
-        font-size: 2rem;
+    .login-container {
+        position: relative;
+        top: 7.5%;
     }
     .runplan-logo-container {
         text-align: center;
         width: 100%;
     }
     .runplan-logo {
-        font-size: 2.5rem;
-        font-family: "Roboto";
-        margin:0;
+        font-size: 3.75rem;
+        margin: 0;
+    }
+    .title-container {
+        text-align: center;
+    }
+    .title {
+        font-size: 2.75rem;
     }
     #sign-in {
+        position: relative;
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -118,71 +131,130 @@ async function submitUserLogin(e) {
     .username-container {
         display: flex;
         flex-direction: column;
-        border-bottom: black 3px solid;
-        margin-bottom: 15%;
-        width:400px;
+        border-bottom: white 3px solid;
+        margin-bottom: 12.5%;
+        margin-top:10%;
+        width:100%;
     }
     .username-label {
         margin-bottom:2.5%;
+        font-weight: 600;
+        font-size: 1.25rem;
     }
     .username-input {
         border: none;
         outline: none;
-        margin-bottom:1%;
-        font-weight: 600;
-        font-family: "Montserrat";
+        margin-bottom:3%;
+        font-size: 1.25rem;
+        background-color: #0387ba
     }
     .password-container {
         display: flex;
         flex-direction: column;
-        border-bottom: black 3px solid;
-        margin-bottom: 15%;
-        width:400px;
+        border-bottom: white 3px solid;
+        margin-bottom: 10%;
+        width:100%;
     }
     .password-label {
+        font-size: 1.25rem;
         margin-bottom:2.5%;
+        font-weight: 600;
     }
     .password-input {
         border: none;
         outline: none;
-        margin-bottom:1%;
+        margin-bottom:3%;
+        font-size: 1.25rem;
+        background-color:#0387ba;
+    }
+    .password-help-container {
+        text-align: start;
+        width:100%;
+        margin-bottom: 5%;
+    }
+    .password-help {
+        font-size: 1rem;
+        font-weight: 600;
+        cursor: pointer;
+        text-decoration: underline;
     }
     .remember-me-container {
         display: flex;
         flex-direction: row;
         align-items: start;
         justify-content: start;
-        margin-bottom: 15%;
-        width:400px;
+        margin-bottom: 10%;
+        width:100%;
+    }
+    .remember-me-checkbox {
+        width:20px;
+        height:20px;
+    }
+    .remember-me-label {
+        font-size: 1.25rem;
+        padding-left:10px;
     }
     .sign-in-button {
         border: none;
-        background-color: black;
-        color: white;
+        background-color: white;
+        color: #0387ba;
         font-size: 1.25rem;
+        font-weight: 800;
         border-radius: 50px;
-        width:400px;
-        height:50px;
+        width:450px;
+        height:60px;
+        cursor: pointer;
+    }   
+    .create-account-container {
+        text-align: center;
+        margin-top:5%;
+    }
+    .create-account {
+        text-decoration: underline;
         cursor: pointer;
     }
 
     @media screen and (max-width: 640px) {
+        .runplan-logo {
+            font-size: 2.75rem;
+            margin: 0;
+        }
+        .title-container {
+            text-align: center;
+        }
+        .title {
+            font-size: 1.75rem;
+        }
         .login-container {
-            height: 80%;
+            height: 90%;
         }
         .username-container {
-            width:325px;
-            margin-bottom: 15%;
+            width:100%;
+        }
+        .username-label {
+            font-size: 1rem;
+        }
+        .username-input {
+            font-size: 1rem;
         }
         .password-container {
-            width:325px;
-            margin-bottom: 15%;
+            width:100%;
+        }
+        .password-label {
+            font-size: 1rem;
+        }
+        .password-input {
+            font-size: 1rem;
         }
         .remember-me-container {
-            width:325px;
+            width:100%;
+        }
+        .remember-me-label {
+            font-size: 1rem;
         }
         .sign-in-button {
-            width:325px;
+            width:100%;
+            height:50px;
         }
     }
 </style>
